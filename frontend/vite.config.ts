f62c05763b14d829729d3ceae2a1463ad9796cc3
Aaron Parker <github@forester.knightmare.org.uk>
2025-12-03 22:38:28 +0000

    feat: alias paths

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
 import {defineConfig} from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@font": path.resolve(__dirname, "./src/assets/fonts"),
+      "@image": path.resolve(__dirname, "./src/assets/images"),
+      "@view": path.resolve(__dirname, "./src/views"),
+    },
+  },
+  plugins: [
+    vue(),
+  ],
 })
